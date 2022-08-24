7e7002030090ab56fd1a905bfab570593d521541
Dwight Awesome <git@theawesomes.net>
2022-08-24 07:35:51 -0400

    fix typos, update comments

--- pullprofiles.py
+++ pullprofiles.py
@@ -5,18 +5,18 @@ from pathlib import Path
 
 
 def updateprofiles():
-    home = str(Path.home()) #for windows
-    credfile = home+'/.aws/config'
-    file = open(credfile, "r")
+    home = str(Path.home()) #Your home dir - at least in windows
+    credfile = home+'/.aws/config' #your aws cred file
+    file = open(credfile, "r") #open the file
     #regex search based on open bracket
-    pattern = "\["
-    list1 = []
+    pattern = "\[" #regex pattern we're going to look for  - open bracket
+    list1 = [] #list that we'll output all the data to
     for line in file:
         if re.search(pattern, line): #if the line starts with a [ bracket
-            line = line.replace("[profile ", "") #remote the "[profile"
-            line = line.replace("]", "")#remote the close bracket]
+            line = line.replace("[profile ", "") #remove the "[profile"
+            line = line.replace("]", "")#remove the close bracket]
             list1.append(line) #append that to the list to return
-    return(list1)
+    return(list1) #return that list
 
     
 if __name__ == "__main__":
